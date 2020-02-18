30311f20809309fae40a6efcccde9d840d54a062
FMGD <55237715+FMGD@users.noreply.github.com>
2020-02-18 09:10:51 +0000

    Exercise 3 b
    Animate only when  move

--- Week5/CMP105App/Zombie.cpp
+++ Week5/CMP105App/Zombie.cpp
@@ -27,7 +27,6 @@ Zombie::~Zombie()
 // Functions
 void Zombie::update(float dt)
 {
-	walk.animate(dt);
 	setTextureRect(walk.getCurrentFrame());
 }
 
@@ -36,20 +35,24 @@ void Zombie::handleInput(float dt)
 	if (input->isKeyDown(sf::Keyboard::Up))
 	{
 		move(0 * dt, -velocity.y * dt);
+		walk.animate(dt);
 	}
 	if (input->isKeyDown(sf::Keyboard::Down))
 	{
 		move(0 * dt, velocity.y * dt);
+		walk.animate(dt);
 	}
 	if (input->isKeyDown(sf::Keyboard::Left))
 	{
 		if (walk.getFlipped() == false) walk.setFlipped(true); // Flipped to left
-		move(-velocity.x * dt, 0 * dt);		
+		move(-velocity.x * dt, 0 * dt);
+		walk.animate(dt);
 	}
 	if (input->isKeyDown(sf::Keyboard::Right))
 	{
 		if (walk.getFlipped() == true) walk.setFlipped(false);  // Not Flipped to left
 		move(velocity.x * dt, 0 * dt);
+		walk.animate(dt);
 	}
 
 }
