597f89656af1a933108daa9eb8a3a7d333fd72e8
FMGD <55237715+FMGD@users.noreply.github.com>
2020-02-18 08:31:30 +0000

    Exercise 1
    Adding Zombie Class to project

---
+++ Week5/CMP105App/Zombie.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Framework/GameObject.h"
+#include "Framework/Animation.h"
+
+class Zombie : public GameObject
+{
+public:
+	// Constructors
+	Zombie();
+
+	// Destructor
+	~Zombie();
+
+	// Functions
+	void update(float dt) override;
+
+protected:
+	Animation walk;
+
+};
+
